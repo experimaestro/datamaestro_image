0bb705e8dec80f010287c62b085674acfe2a345c
Benjamin Piwowarski <benjamin@piwowarski.fr>
2020-09-24 13:50:43 +0200

    Reformat

--- datamaestro_image/__init__.py
+++ datamaestro_image/__init__.py
@@ -1,5 +1,6 @@
 import datamaestro
 
+
 class Repository(datamaestro.Repository):
     AUTHOR = """Benjamin Piwowarski <benjamin@piwowarski.fr>"""
     DESCRIPTION = """Image-related datasets repository"""
--- datamaestro_image/data/__init__.py
+++ datamaestro_image/data/__init__.py
@@ -3,18 +3,24 @@ from datamaestro.data.ml import Supervised
 from datamaestro.definitions import data, argument, datatasks, datatags, dataset
 from datamaestro.data.tensor import IDX
 
-@data()
-class Image(Base): pass
 
 @data()
-class IDXImage(IDX, Image): pass
+class Image(Base):
+    pass
+
+
+@data()
+class IDXImage(IDX, Image):
+    pass
+
 
 @datatasks("image classification")
 @datatags("images")
 @data()
 class ImageClassification(Supervised):
-  """Image classification dataset"""
-  pass
+    """Image classification dataset"""
+
+    pass
 
 
 @argument("images", Image)
@@ -23,6 +29,6 @@ class ImageClassification(Supervised):
 @datatags("images")
 @data()
 class LabelledImages(Base):
-  """Image classification dataset"""
-  pass
+    """Image classification dataset"""
 
+    pass
